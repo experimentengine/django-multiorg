be4bbeb947531bd3954a281c5954dcd2c573308c
Jeremy Boyd <jeremy.boyd@experimentengine.com>
2016-02-03 14:12:40 -0600

    Don't send request to _adopt_org

--- multiorg/views.py
+++ multiorg/views.py
@@ -36,7 +36,9 @@ class MultiorgAdoptionView(View):
         user_organizations = getattr(self.request.user,
                                      settings.MULTIORG_ORGANIZATION_ATTR)
         if organization not in user_organizations.all():
+            logger.debug('Requested organization %s not in user orgs %s', organization_id, user_organizations.all())
             if not (ADMINS_ADOPT_ANY and self.request.user.is_staff):
+                logger.debug('Admins can adopt any: %s, user is staff: %s', ADMINS_ADOPT_ANY, self.request.user.is_staff)
                 return HttpResponseForbidden()
 
         # At this point, we know the user is allowed to adopt an existing organization.
@@ -48,11 +50,11 @@ class MultiorgAdoptionView(View):
         organization_id = self.request.POST.get('organization_id')
         redirect_destination = self.request.POST.get(NEXT_URL_PARAM_NAME,
                                                      MULTIORG_REDIRECT_URL)
-        return self._adopt_org(request, organization_id, redirect_destination)
+        return self._adopt_org(organization_id, redirect_destination)
 
     def get(self, request):
-        logger.debug('Got GET: %s', self.request.POST)
+        logger.debug('Got GET: %s', self.request.GET)
         organization_id = self.request.GET.get('organization_id')
         redirect_destination = self.request.GET.get(NEXT_URL_PARAM_NAME,
                                                     MULTIORG_REDIRECT_URL)
-        return self._adopt_org(request, organization_id, redirect_destination)
+        return self._adopt_org(organization_id, redirect_destination)
